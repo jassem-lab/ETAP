2a28730266a1232d84d8157a195e85e50890bcd5
jassem-lab <jassemgaaloul123@gmail.com>
2021-04-29 14:48:02 +0100

    🔥 Passport Config & working on user auth

--- server/src/api/users.js
+++ server/src/api/users.js
@@ -1,6 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+// Loading input validation
 
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
+
+const User = require('../models/user.model');
+
+// @access Public
+// @desc Register User
+// @Route Post api/users/register
+
+router.post('/', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+});
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -55,5 +55,5 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const userModel = mongoose.model('users', userSchema);
+const userModel = mongoose.model('user', userSchema);
 module.exports = userModel;
